e27a73e8c670512976134aa3c47ff3905990322c
Felipe Leusin <felipe.leusin@gmail.com>
2012-12-11 22:40:55 -0200

    created project structure

---
+++ src/RavenDb.AttributeTags/RavenDb.AttributeTags/RavenTagAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RavenDb.AttributeTags
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+    public class RavenTagAttribute : Attribute
+    {
+        public string Name { get; set; }
+
+        public RavenTagAttribute(string name)
+        {
+            this.Name = name;
+        }
+    }
+}
---
+++ src/RavenDb.AttributeTags/RavenDb.AttributeTags/RavenTagHelpers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace RavenDb.AttributeTags
+{
+    public static class RavenTagHelpers
+    {
+        public static readonly ConcurrentDictionary<Type, string> TypeTagsCache = new ConcurrentDictionary<Type, string>();
+
+        public static string GetTag(Type t)
+        {
+            string tagName;
+            if ( !TypeTagsCache.TryGetValue(t, out tagName) )
+            {
+                var tagAttributes = t.GetCustomAttributes(typeof (RavenTagAttribute), true);
+                if (tagAttributes.Length == 0)
+                {
+                    return null;
+                }
+                tagName = TypeTagsCache.GetOrAdd(t, ((RavenTagAttribute)tagAttributes[0]).Name);
+            }
+
+            return tagName;
+        }
+    }
+}
